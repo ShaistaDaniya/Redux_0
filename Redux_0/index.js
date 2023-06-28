a5e8b0689450e02be9082d6850eddbe7518999ff
ShaistaDaniya <shaista@gigandtake.com>
2023-06-28 15:01:04 +0530

    #16

--- Redux_0/index.js
+++ Redux_0/index.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './Components/reducer'; // Replace with your root reducer
 import App from './App'; // Replace with your main application component
+import { AppRegistry, Platform } from 'react-native';
 
 const store = createStore(rootReducer); // Create your Redux store with the root reducer
 
@@ -14,4 +15,12 @@ const Root = () => {
   );
 };
 
-export default Root;
+// Register the component for React Native
+AppRegistry.registerComponent('X', () => Root);
+
+// Check the platform and run the application accordingly
+if (Platform.OS === 'web') {
+  const rootTag = document.getElementById('root') || document.getElementById('X');
+  AppRegistry.runApplication('X', { rootTag });
+}
+
