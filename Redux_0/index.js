5ffa9041d7e581f01425eb5a4f81d78b2f9f326d
ShaistaDaniya <shaista@gigandtake.com>
2023-06-28 15:15:49 +0530

    #18

--- Redux_0/index.js
+++ Redux_0/index.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './Components/reducer'; // Replace with your root reducer
-import App from './App'; // Replace with your main application component
-import { AppRegistry, Platform } from 'react-native';
+/**
+ * @format
+ */
 
-const store = createStore(rootReducer); // Create your Redux store with the root reducer
+import {AppRegistry} from 'react-native';
+import App from './Components/App';
 
-const Root = () => {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-};
+import {name as appName} from './app.json';
 
-// Register the component for React Native
-AppRegistry.registerComponent('X', () => Root);
-
-// Check the platform and run the application accordingly
-if (Platform.OS === 'web') {
-  const rootTag = document.getElementById('root') || document.getElementById('X');
-  AppRegistry.runApplication('X', { rootTag });
-}
-
+AppRegistry.registerComponent(appName, () => App);
